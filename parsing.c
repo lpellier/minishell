c28c8bc936d9b11fc2e0d32318073e8839e8b607
lpellier <pellierlucas@gmail.com>
2021-03-09 16:18:48 +0100

    added  feature

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: lpellier <lpellier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 22:24:47 by lpellier          #+#    #+#             */
-/*   Updated: 2021/03/09 00:04:39 by lpellier         ###   ########.fr       */
+/*   Updated: 2021/03/09 15:47:43 by lpellier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int			get_input(char *line, t_cmd *cmd)
 	index = 0;
 	while (line[index] && pipe_or_colon(line[index]))
 		index++;
-	if (!(cmd->input = malloc(sizeof(char) * index + 1)))
+	if (!(cmd->input = malloc(sizeof(char) * (index + 1))))
 		return (FAILURE);
 	index = 0;
 	while (line[index] && pipe_or_colon(line[index]))
@@ -70,7 +70,7 @@ int			spaces(char *s)
 
 	i = 0;
 	count = 0;
-	while (!is_whitespace(s[i]))
+	while (s[i] && !is_whitespace(s[i]))
 	{
 		count++;
 		i++;
@@ -114,14 +114,102 @@ void		read_cmd(char *line, t_info *info, int index, int index_cmd)
 	if (cmd->cmd && cmd->bui == 9)
 		info->output = ft_strjoin(ft_strjoin("minisheh: ", cmd->cmd),
 			": command not found\n");
-	else if (cmd->bui == 8 && !pipe_or_colon(line[index]))
-		pipe_for_exec(info, index_cmd, line, index, 1);
-	else if (cmd->bui == 8)
-		pipe_for_exec(info, index_cmd, line, index, 0);
-	else if (!cmd->cmd)
+	else if (cmd->bui == 9)
 		info->output = ft_strdup(""); /* might cause an issue later */
+	else if (!pipe_or_colon(line[index]))
+		pipe_for_exec(info, index_cmd, line, index, 1);
+	else if (pipe_or_colon(line[index]))
+		pipe_for_exec(info, index_cmd, line, index, 0);
 	else
 		(*built_in[cmd->bui])(info, index_cmd);
+}
+
+/*
+** replaces every $ with its env variable counterpart
+*/
+
+#include <string.h>
+
+// You must free the result if result is non-NULL.
+char *str_replace(char *orig, char *rep, char *with) {
+    char *result; // the return string
+    char *ins;    // the next insert point
+    char *tmp;    // varies
+    int len_rep;  // length of rep (the string to remove)
+    int len_with; // length of with (the string to replace rep with)
+    int len_front; // distance between rep and end of last rep
+    int count;    // number of replacements
+
+    // sanity checks and initialization
+    if (!orig || !rep)
+        return NULL;
+    len_rep = strlen(rep);
+    if (len_rep == 0)
+        return NULL; // empty rep causes infinite loop during count
+    if (!with)
+        with = "";
+    len_with = strlen(with);
+
+    // count the number of replacements needed
+    ins = orig;
+    for (count = 0; (tmp = strstr(ins, rep)); ++count) {
+        ins = tmp + len_rep;
+    }
+
+    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+    if (!result)
+        return NULL;
+
+    // first time through the loop, all the variable are set correctly
+    // from here on,
+    //    tmp points to the end of the result string
+    //    ins points to the next occurrence of rep in orig
+    //    orig points to the remainder of orig after "end of rep"
+    while (count--) {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, orig);
+    return result;
+}
+
+
+char *replace_dollars_env(t_info *info, char *line)
+{
+	char *test;
+	char *dst;
+	t_list *value;
+	char *cmp;
+	int i;
+
+	test = ft_strdup(line);
+	while ((test = strstr(test, "$")))
+	{
+		if (test[1] && is_whitespace(test[1]) && test[1] != '$')
+		{
+			i = 1;
+			while (test[i] && is_whitespace(test[i]) && test[i] != '$')
+				i++;
+			if (!(dst = malloc(sizeof(char) * i)))
+				return (NULL);
+			strncpy(dst, test, i);
+			dst[i] = '\0';
+			value = (ft_list_find(info->env_head,
+				create_env_struct(dst + 1, "NULL"), cmp_env));
+			if (value && (cmp = ft_strdup(((t_env *)value->data)->value)))
+			{
+			    line = str_replace(line, dst, cmp);
+			    free(cmp);
+			 	cmp = NULL;
+			}
+		}
+		test++;
+	}
+	return (line);
 }
 
 /* reads line using gnl and feeds t_cmd linked lists */
@@ -129,9 +217,11 @@ void		read_cmd(char *line, t_info *info, int index, int index_cmd)
 void		read_line(t_info *info)
 {
 	char	*line;
+	char	*true_line;
 
 	get_next_line(0, &line);
-	read_cmd(line, info, 0, 0);
+	true_line = replace_dollars_env(info, line);
+	read_cmd(true_line, info, 0, 0);
 	if (line)
 		free(line);
 	line = NULL;
