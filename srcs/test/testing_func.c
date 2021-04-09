418e99c0254e422e8e442e8864dce442bac61f31
Téo Froissart <teofroissart@mbp-de-teo.home>
2021-04-09 14:36:54 +0200

    norme of test directory

--- srcs/test/testing_func.c
+++ srcs/test/testing_func.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   testing_func.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpellier <lpellier@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tefroiss <tefroiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 11:45:07 by lpellier          #+#    #+#             */
-/*   Updated: 2021/03/24 16:06:29 by lpellier         ###   ########.fr       */
+/*   Updated: 2021/04/09 14:35:44 by tefroiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void		print_cmd_info(t_cmd *cmd)
+void	print_cmd_info(t_cmd *cmd)
 {
 	ft_printf(CYAN "'%s'" RESET, cmd->cmd);
 	ft_printf(RED " | " RESET);
