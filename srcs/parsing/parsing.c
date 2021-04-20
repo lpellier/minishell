08f30f480d810a3dc4b54e5578c7d1c26efe66fa
lpellier <pellierlucas@gmail.com>
2021-04-20 14:54:06 +0200

    test

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: lpellier <lpellier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 22:24:47 by lpellier          #+#    #+#             */
-/*   Updated: 2021/04/20 13:27:48 by lpellier         ###   ########.fr       */
+/*   Updated: 2021/04/20 14:53:49 by lpellier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,7 @@ int	ft_set_index(char *line, t_cmd *cmd, int index)
 	index += spaces(&line[index], index);
 	index += get_option(&line[index], cmd, index);
 	index += spaces(&line[index], index);
+	ft_printf("test");
 	index += get_input(&line[index], cmd, index);
 	index += spaces(&line[index], index);
 	return (index);
