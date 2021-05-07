f8ad219b0efab95ea4cd02655fc6504bc7fd51bf
lpellier <pellierlucas@gmail.com>
2021-05-07 15:44:28 +0200

    itoa normed

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,11 +6,29 @@
 /*   By: lpellier <lpellier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 10:48:12 by lpellier          #+#    #+#             */
-/*   Updated: 2021/05/07 15:30:45 by lpellier         ###   ########.fr       */
+/*   Updated: 2021/05/07 15:43:48 by lpellier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	assign_nbrs(int nbr, long *n, size_t *len)
+{
+	*n = nbr;
+	*len = 0;
+	if (*n < 0)
+		*len = 1;
+	if (*n < 0)
+		*n *= -1;
+	if (!nbr)
+		(*len)++;
+	while (nbr)
+	{
+		(*len)++;
+		if (*len)
+			nbr /= 10;
+	}
+}
 
 char	*ft_itoa(int nbr)
 {
@@ -18,16 +36,8 @@ char	*ft_itoa(int nbr)
 	size_t	len;
 	char	*str;
 
-	n = nbr;
-	len = 0;
-	if (n < 0)
-		len = 1;
-	if (n < 0)
-		n = -n;
-	while (nbr)
-		nbr = len++ ? nbr / 10 : nbr / 10;
-	str = (char *)malloc(sizeof(str) * len + 1);
-	if (!str)
+	assign_nbrs(nbr, &n, &len);
+	if (ft_calloc((void **)&str, len + 1, sizeof(str)))
 		return (NULL);
 	*(str + len--) = '\0';
 	while (n > 0)
